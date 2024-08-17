321bff87a3a558f0510533d950b45b779e1175c5
Tin <tin@Tins-MacBook-Air.local>
2024-08-17 13:24:35 +0530

    added monitoring

---
+++ Terraform/Monitoring/versions.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.61.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.12.1"
+    }
+  }
+  backend "s3" {
+    bucket  = "microservices-tf-state"
+    encrypt = true
+    key     = "MICROSERVICES-SRE/microservices/terraform/Monitoring/terraform.tfstate"
+    region  = "us-east-2"
+  }
+}
+
+provider "aws" {
+  region = var.region
+
+  default_tags {
+    tags = {
+      owner = "tfadmin"
+    }
+  }
+}
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.eks_cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_autority[0].data)
+    token                  = data.aws_eks_cluster_auth.eks_cluster_auth.token
+  }
+}
+
+variable "region" {
+  description = "aws region"
+  default     = "us-east-2"
+}
+
+data "aws_eks_cluster" "eks_cluster" {
+  name = local.name
+}
+
+data "aws_eks_cluster_auth" "eks_cluster_auth" {
+  name = local.cluster_name
+}
